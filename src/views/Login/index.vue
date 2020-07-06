700f2a2558f1897419f5e100d5ee7732f01ac057
hiywin <hellowin1853@gmail.com>
2020-07-06 22:47:09 +0800

    配置全局样式

---
+++ src/views/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+    这里是登陆页
+    <el-button type="primary">默认按钮</el-button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
