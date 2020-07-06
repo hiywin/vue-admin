a120e2b593e2881282bde9bf28a536fbb9645367
hiywin <hellowin1853@gmail.com>
2020-07-06 23:34:20 +0800

    登陆界面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,15 +1,62 @@
 <template>
   <div id="login">
-    这里是登陆页
-    <el-button type="primary">默认按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMenu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  name: "login",
+  data() {
+    return {
+      menuTab: [
+        { txt: "登陆", current: true },
+        { txt: "注册", current: false }
+      ]
+    };
+  },
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(elem => {
+        elem.current = false;
+      });
+      data.current = true;
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 #login {
   height: 100vh;
   background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
