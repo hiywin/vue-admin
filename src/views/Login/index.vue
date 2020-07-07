a8782e704de13a015a8fa4ffaae6c3b0a25290af
hiywin <hellowin1853@gmail.com>
2020-07-08 00:16:32 +0800

    封装数据校验方法

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -31,8 +31,22 @@
         <el-form-item prop="password" class="item-form">
           <label>密码</label>
           <el-input
-            type="password"
+            type="text"
             v-model="ruleForm.password"
+            autocomplete="off"
+            minlength="6"
+            maxlength="20"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          prop="passwords"
+          class="item-form"
+          v-show="model === 'register'"
+        >
+          <label>重复密码</label>
+          <el-input
+            type="text"
+            v-model="ruleForm.passwords"
             autocomplete="off"
             minlength="6"
             maxlength="20"
@@ -43,7 +57,7 @@
           <el-row :gutter="10">
             <el-col :span="15">
               <el-input
-                v-model.number="ruleForm.code"
+                v-model="ruleForm.code"
                 minlength="6"
                 maxlength="6"
               ></el-input>
@@ -66,15 +80,20 @@
   </div>
 </template>
 <script>
+import {
+  stripscript,
+  validateEmail,
+  validatePwd,
+  validateVCode
+} from "@/utils/validate";
 export default {
   name: "login",
   data() {
     // 验证用户名
     var validateUserName = (rule, value, callback) => {
-      let reg = /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
       if (value === "") {
         callback(new Error("请输入用户名"));
-      } else if (!reg.test(value)) {
+      } else if (validateEmail(value)) {
         callback(new Error("用户名格式有误"));
       } else {
         callback();
@@ -82,21 +101,39 @@ export default {
     };
     // 验证密码
     var validatePassword = (rule, value, callback) => {
-      let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+      this.ruleForm.password = stripscript(value);
+      value = this.ruleForm.password;
       if (value === "") {
         callback(new Error("请输入密码"));
-      } else if (!reg.test(value)) {
+      } else if (validatePwd(value)) {
         callback(new Error("密码为6至20位数字+字母"));
+      } else {
+        callback();
+      }
+    };
+    // 验证密码
+    var validatePasswords = (rule, value, callback) => {
+      // 如果模块为login时，直接通过
+      if (this.model === "login") {
+        callback();
+      }
+      this.ruleForm.passwords = stripscript(value);
+      value = this.ruleForm.passwords;
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value != this.ruleForm.password) {
+        callback(new Error("重复密码不正确"));
       } else {
         callback();
       }
     };
     // 验证验证码
     var validateCode = (rule, value, callback) => {
-      let reg = /^[a-z0-9]{6}$/;
+      this.ruleForm.code = stripscript(value);
+      value = this.ruleForm.code;
       if (value === "") {
         return callback(new Error("请输入验证码"));
-      } else if (!reg.test(value)) {
+      } else if (validateVCode(value)) {
         return callback(new Error("验证码格式有误"));
       } else {
         callback();
@@ -104,17 +141,21 @@ export default {
     };
     return {
       menuTab: [
-        { txt: "登陆", current: true },
-        { txt: "注册", current: false }
+        { txt: "登陆", current: false, type: "login" },
+        { txt: "注册", current: true, type: "register" }
       ],
+      // 模块
+      model: "register",
       ruleForm: {
         username: "",
         password: "",
+        passwords: "",
         code: ""
       },
       rules: {
         username: [{ validator: validateUserName, trigger: "blur" }],
         password: [{ validator: validatePassword, trigger: "blur" }],
+        passwords: [{ validator: validatePasswords, trigger: "blur" }],
         code: [{ validator: validateCode, trigger: "blur" }]
       }
     };
@@ -125,6 +166,17 @@ export default {
         elem.current = false;
       });
       data.current = true;
+      this.model = data.type;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     }
   }
 };
