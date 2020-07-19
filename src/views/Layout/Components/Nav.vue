d0e1c303bf96faeb697d47704eb031360cf6425c
hiywin <hellowin1853@gmail.com>
2020-07-19 23:32:16 +0800

    导航样式、路由设置

--- src/views/Layout/Components/Nav.vue
+++ src/views/Layout/Components/Nav.vue
@@ -1,10 +1,62 @@
 <template>
   <div id="nav-wrap">
-    导航
+    <el-menu
+      default-active="1-4-1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+      background-color="transparent"
+      text-color="#fff"
+      active-text-color="#fff"
+      router
+    >
+      <template v-for="(item, index) in routers">
+        <el-submenu v-if="!item.hidden" :key="item.id" :index="index">
+          <!--一级菜单-->
+          <template slot="title">
+            <i :class="item.meta.icon"></i>
+            <span slot="title">{{ item.meta.name }}</span>
+          </template>
+          <!--子级菜单-->
+          <el-menu-item
+            v-for="subItem in item.children"
+            :key="subItem.id"
+            :index="subItem.path"
+            >{{ subItem.meta.name }}</el-menu-item
+          >
+        </el-submenu>
+      </template>
+    </el-menu>
   </div>
 </template>
 <script>
-export default {};
+import { ref, reactive } from "@vue/composition-api";
+export default {
+  name: "navMenu",
+  setup(props, { root }) {
+    // data数组
+    const isCollapse = ref(false);
+    // 路由数组
+    const routers = reactive(root.$router.options.routes);
+    console.log(routers);
+
+    // 定义函数
+    const handleOpen = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
+    const handleClose = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
+
+    return {
+      isCollapse,
+      handleOpen,
+      handleClose,
+      routers
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
 @import "../../../styles/config.scss";
